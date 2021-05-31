d070af954c8f76735b17e87a02a5daf79b80d1d4
user <bipinaghimire6@gmail.com>
2021-05-31 09:44:46 +0545

    area

---
+++ Labone/area.py
@@ -0,0 +1,8 @@
+'''
+2. Write a program thet reads the length of base and the height of right angled triangle and prints the are. Every number
+is given of seperate line
+'''
+base = 10
+height = 20
+area = (1/2)*base*height
+print (f'The area of given right angled triangle is{area}')
